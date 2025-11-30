f7cd8478362e7020cb5a1f6e9bd5e94a542eb46d
Quissola11935 <osvaldo@demaria.com.br>
2025-11-30 11:10:33 +0100

    qwwerti

--- lib/core/api/api_constant.dart
+++ lib/core/api/api_constant.dart
@@ -1,9 +1,10 @@
-const BASE_URL = "10.30.203.26:8000/api/v1";
+const BASE_URL = "192.168.0.48:8000/api/v1";
+//"optimus-game-1089660016.us-east-1.elb.amazonaws.com/api/v1";
 // "10.30.203.26:8000/api/v1";
 //"optimus-game-1089660016.us-east-1.elb.amazonaws.com/api/v1";
 //44.197.208.171:8000/api/v1
 //"44.211.86.113:8000/api/v1";
-//"192.168.8.181:8000/api/v1";
+//"192.168.0.48:8000/api/v1";
 //glorious-enchantment-production-6b44.up.railway.app
 //"mathgamebackend-production.up.railway.app/api/v1";
 // https://83d2-2c0f-f888-a001-48a0-505e-2903-97b5-3c45.ngrok-free.app
